92568793b593f7ee3e07646f65deb50f89b2af0c
Jonathan Kaneshiro <jkaneshiro97@gmail.com>
2019-10-11 22:34:41 -0700

    added miniproject

---
+++ lesson03/math_eqs_tomas.py
@@ -0,0 +1,23 @@
+# Math equations python script
+
+# import math
+# https://docs.python.org/3/library/math.html
+
+from math import pi
+# FROM -  imports a subset or even specific methods from that module
+# IMPORT - imports the module X and creates a reference
+
+# def - defines a function in python
+# pass - nothing happens -- passes that function
+
+def area_of_circle():
+	# TODO
+	# ask the user to input the radius
+	# calculate the area of a circle
+	# print out the area using str.format
+	pass
+	
+def main():
+	area_of_circle()
+
+main()
