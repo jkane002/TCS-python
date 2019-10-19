b5acec43fc74bbc9d75b9e12e4f97e8dee104cb8
Jonathan Kaneshiro <jkaneshiro97@gmail.com>
2019-10-18 17:20:39 -0700

    added review03

---
+++ lesson04/review03.py
@@ -0,0 +1,25 @@
+"""
+AND - what must be true for the statement to be true?
+OR - what must be true for the statement to be true?
+"""
+
+def and_operator(x):
+    # check if x is less than 5 and less than 10
+    # print True if it is, print False otherwise
+
+def or_operator(x):
+    # check if x is less than 4 or less than 5
+    # print True if it is, print False otherwise
+
+
+def main():
+    and_operator(5)
+    or_operator(3.0)
+
+main()
+
+"""
+1. True and (True or False)
+2. (True and False) or (True or Not(False))
+3. ((False and True) or (False or Not(True)))
+"""
