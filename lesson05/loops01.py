831469f95731395fa9f3aaf7ef3a6fcd504940da
Jonathan Kaneshiro <jkaneshiro97@gmail.com>
2019-10-26 18:32:58 -0700

    added loops file

---
+++ lesson05/loops01.py
@@ -0,0 +1,39 @@
+def one():
+    print("# 1. Fill in the parameters to range, so that this loop prints the numbers 20, 22, 24, 26, and 28.")
+    # for i in range(1, 1, 1):
+    #     print(i)
+
+def two():
+    print("# 2. How many times would this loop execute?")
+    # j = 10
+    # while j > 0:
+    #     print(j)
+    #     j -= 1
+
+def three():
+    print("# 3. How would you change this code to make it not be an infinite loop?")
+    # k = 10
+    # while k > 0:
+    #     print(k)
+    #     k = k + 1 #is there another way to write this?
+
+def four():
+    print("# 4. Change this loop from a for to a while loop")
+    # for l in range(1, 10):
+    #     print(l)
+
+def five():
+    print("# 5. How many times would this loop execute?")
+    # m = 100
+    # while m > 10:
+    #     print(m)
+    #     m = m / 1
+
+def main():
+    one()
+    two()
+    three()
+    four()
+    five()
+
+main()
