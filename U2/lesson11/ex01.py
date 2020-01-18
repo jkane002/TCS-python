7e7f83e84ba9ed860fedb8818bf0fbc768ab99e8
Jonathan Kaneshiro <jkaneshiro97@gmail.com>
2020-01-17 21:16:49 -0800

    added ex01

---
+++ U2/lesson11/ex01.py
@@ -0,0 +1,12 @@
+'''
+Suppose we are writing code to track different types of cars in a race. There
+are three types of cars, a sports car, a van, and a truck. For one move, a
+sports car moves 5 units forward, a van 3, and a truck 1. Each move uses
+one unit of gas; and a sports car starts with 10 gallons, and van with 15 and
+a truck with 25. Write some code using factory methods that tracks the gas and units
+travelled of each vehicle.
+
+Constraints
+Assume you will never go into negative gas
+'''
+class Car():
