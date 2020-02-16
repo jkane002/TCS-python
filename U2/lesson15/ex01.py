908d21992bc4cc688e9a87cf5c9b71cd24c425da
Jonathan Kaneshiro <jkaneshiro97@gmail.com>
2020-02-15 16:45:21 -0800

    lesson 15

---
+++ U2/lesson15/ex01.py
@@ -0,0 +1,6 @@
+'''
+Create the parent class vehicle that has a color, price, and gas attributes.
+The parent class also has methods like fillUpTank, emptyTank, and gasLeft.
+Make children classes truck and car that inherit from the parent class. Create
+their own methods.
+'''
---
+++ U2/lesson15/ex01_solution.py
@@ -0,0 +1,32 @@
+class Vehicle:
+    def __init__(self, price, color):
+        self.color = color
+        self.price = price
+        self.gas = 0
+
+    def fillUpTank(self):
+        self.gas = 100
+
+    def emptyTank(self):
+        self.gas = 0
+
+    def gasLeft(self):
+        return self.gas
+
+
+class Truck(Vehicle):
+    def __init__(self, price, color, tires):
+        super().__init__(price, color)
+        self.tires = tires
+
+    def beep(self):
+        print("Honk honk")
+
+
+class Car(Vehicle):
+    def __init__(self, price, color, speed):
+        super().__init__(price, color)
+        self.speed = speed
+
+    def beep(self):
+        print("Beep Beep")
