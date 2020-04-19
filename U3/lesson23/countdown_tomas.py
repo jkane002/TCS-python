ffbdb34fe422716f41dbde41165ebfa79f62babf
Jonathan Kaneshiro <jkaneshiro97@gmail.com>
2020-04-18 17:45:02 -0700

    added lessons

---
+++ U3/lesson23/countdown_tomas.py
@@ -0,0 +1,11 @@
+# Countdown using a for loop
+def Countdown_for(n):
+
+# Countdown using recursion
+def Countdown_recursion(n):
+
+
+print("For Loop")
+Countdown_for(5)
+print("Recursion")
+Countdown_recursion(5)
---
+++ U3/lesson23/factorial_tomas.py
@@ -0,0 +1,10 @@
+#recursive
+def factorial_rec(n):
+
+
+# Iterative
+def factorial_it(n):
+
+
+print(factorial_it(7))
+print(factorial_rec(7))
